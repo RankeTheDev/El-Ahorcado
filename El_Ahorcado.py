49039fe689b968ca049669f53c30e95c60125b4c
Christian AC <chrisalco06@gmail.com>
2025-03-24 21:45:55 +0100

    Se convirtió toda la creación del grid en una función

--- El_Ahorcado.py
+++ El_Ahorcado.py
@@ -6,18 +6,18 @@ from Palabras import words_list
 from Fases_Ahorcado import Fases_ahorcado
 
 # LOGICA PARA LA PALABRA AL AZAR
-    # BASE 
+# BASE 
 word = random.choice(words_list)
 hidden_word = ["_"] * len(word)
 chances = 5
 failed_letters = list()
 guess_word = False
 
-    # TEST FUNCIÓN PALABRA OCULTA
+# TEST FUNCIÓN PALABRA OCULTA
 print(hidden_word, word)
 
-    # DIBUJO AHORCADO
-def if_dibujo_ahorcado():
+# DIBUJO AHORCADO
+def Dibujo_Ahorcado():
     if chances == 5:
         return Fases_ahorcado[0]
 
@@ -35,11 +35,8 @@ def if_dibujo_ahorcado():
 
     elif chances == 0:
         return Fases_ahorcado[5]
-    
-Fase_ahorcado = if_dibujo_ahorcado()
-
+Fase_ahorcado = Dibujo_Ahorcado()
 print(Fase_ahorcado)
-
 
 # SET-UP DE LA VENTANA
 window = ttk.Window(themename="cyborg")
@@ -47,11 +44,33 @@ window.title("EL AHORCADO")
 window.attributes("-fullscreen", True)
 window.bind("<Escape>", lambda event: window.destroy())
 
-# WIDGETS
+# CONFIGURO EL GRID
+def Crear_Grid():
+    # COLUMN CONFIGURE
+    window.columnconfigure((0, 9), weight= 3)
+    window.columnconfigure((1), weight= 15)
+    window.columnconfigure((2), weight= 11)
+    window.columnconfigure((3), weight= 13)
+    window.columnconfigure((4), weight= 10)
+    window.columnconfigure((5), weight= 5)
+    window.columnconfigure((6), weight= 27)
+    window.columnconfigure((7), weight= 10)
+    window.columnconfigure((8), weight= 4)
+
+    # ROW CONFIGURE
+    window.rowconfigure((0,6), weight= 4)
+    window.rowconfigure((1), weight= 21)
+    window.rowconfigure((2), weight= 15)
+    window.rowconfigure((3), weight= 18)
+    window.rowconfigure((4), weight= 19)
+    window.rowconfigure((5), weight= 19)
+Crear_Grid()
+
+# CREO LOS WIDGETS
 Label_Tittle = ttk.Label(window, text = "EL AHORCADO", background= "blue", font = "Cambria 25 bold", anchor= "center")
 Label_Guess = ttk.Label(window, font= "Calibri 14", text = "Introduzca una letra o palabra para adivinar:", background= "green", anchor= "center")
 Entry_Guess = ttk.Entry(window, font= "Calibri 20", foreground= "green", justify="center")
-Button_Submit = ttk.Button(window, text="Enviar", command= lambda: print(chances))
+Button_Submit = ttk.Button(window, text="Enviar", command= Boton_Enviar)
 Label_Failed_Guesses = ttk.Label(window, font= "Calibri 14", text = "Estas letras y palabras son incorrectas:", background= "red", anchor= "center")
 Label_Failed_Guesses_List = ttk.Label(window, font= "Calibri 14", text = failed_letters, background= "red", anchor= "center")
 Label_Chances = ttk.Label(window, font= "Calibri 14", text = "Intentos restantes", background= "brown", anchor= "center") #Fondo dinámico
@@ -60,29 +79,7 @@ Label_Hangman = ttk.Label(window, font= "Calibri 14", text = Fase_ahorcado, back
 Label_Hidden_Word = ttk.Label(window, font= "Calibri 20", text = "Esta es la palabra oculta", anchor= "center")
 Label_Hidden_Word_Show = ttk.Label(window, font= "Calibri 36", text = hidden_word, anchor= "center")
 
-
-# CONFIGURO ROWS Y COLUMNS
-    # COLUMN CONFIGURE
-window.columnconfigure((0, 9), weight= 3)
-window.columnconfigure((1), weight= 15)
-window.columnconfigure((2), weight= 11)
-window.columnconfigure((3), weight= 13)
-window.columnconfigure((4), weight= 10)
-window.columnconfigure((5), weight= 5)
-window.columnconfigure((6), weight= 27)
-window.columnconfigure((7), weight= 10)
-window.columnconfigure((8), weight= 4)
-
-    # ROW CONFIGURE
-window.rowconfigure((0,6), weight= 4)
-window.rowconfigure((1), weight= 21)
-window.rowconfigure((2), weight= 15)
-window.rowconfigure((3), weight= 18)
-window.rowconfigure((4), weight= 19)
-window.rowconfigure((5), weight= 19)
-
-# PONGO LOS ELEMNTOS EN EL GRID
-
+# PONGO LOS WIDGETS EN EL GRID
 Label_Tittle.grid(row= 1, column= 1, columnspan= 8, sticky= "nsew", padx= 5, pady= 5)
 Label_Guess.grid(row= 2, column= 2, sticky= "nsew", padx= 5, pady= 40)
 Entry_Guess.grid(row= 2 , column= 4, columnspan= 3, sticky= "nsew", padx= 10, pady= 40)
@@ -94,7 +91,6 @@ Label_ChancesNumber.grid(row= 5 , column= 2, columnspan= 1, sticky= "nsew", padx
 Label_Hangman.grid(row= 4, column= 3, rowspan= 2, columnspan= 2, sticky= "nsew", padx= 5, pady= 50)
 Label_Hidden_Word.grid(row= 4, column= 6, columnspan= 2, sticky= "nsew", padx= 5, pady= 5)
 Label_Hidden_Word_Show.grid(row= 5, column= 6, columnspan= 2, sticky= "nsew", padx= 5, pady= 5)
-
 
 # RUN
 window.mainloop()
